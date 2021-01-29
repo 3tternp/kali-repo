a6d179271eb935d73fecf4497e4320b603c85bab
astra <punkx.0gravity@gmail.com>
2021-01-29 06:52:09 +0000

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -12,9 +12,9 @@ read count
  
 if [ $count -eq 1 ]
 then
-echo "deb http://http.kali.org/kali kali-last-snapshot main non-free contrib" > /etc/apt/sources.list
-echo "# For source package access, uncomment the following line">>/etc/apt/sources.list
 echo "deb http://http.kali.org/kali kali-rolling main non-free contrib">>/etc/apt/sources.list
+echo "# For latest and safest snapshot access, uncomment the following line">>/etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-last-snapshot main non-free contrib" > /etc/apt/sources.list
 exit
 
 elif [ $count -eq 2 ]
